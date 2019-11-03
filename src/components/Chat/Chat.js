4413a52718aa8e0cd08900daeb164d16d536b53e
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-11-03 23:04:30 +0545

    online users displayed

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,12 +7,14 @@ import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
@@ -38,6 +40,16 @@ const Chat = ({ location }) => {
     socket.on('message', message => {
       setMessages([...messages, message]);
     });
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    });
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    };
   }, [messages]);
 
   const sendMessage = event => {
@@ -61,6 +73,7 @@ const Chat = ({ location }) => {
           sendMessage={sendMessage}
         />
       </div>
+      <TextContainer users={users} />
     </div>
   );
 };
